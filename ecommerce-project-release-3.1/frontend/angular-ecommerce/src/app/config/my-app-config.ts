c74f0902bec85e4222484ad4d26ba9628886e3ae
Muhammed-Javith <muhammadjavith.a@gmail.com>
2023-10-19 19:39:28 +0530

    release3 source code added

---
+++ ecommerce-project-release-3.1/frontend/angular-ecommerce/src/app/config/my-app-config.ts
@@ -0,0 +1,16 @@
+export default {
+
+    oidc: {
+        clientId: '<<UPDATE-WITH-YOUR-APP-CLIENT-ID>>',
+        issuer: 'https://<<UPDATE-WITH-YOUR-DEV-DOMAIN>>/oauth2/default',
+        redirectUri: 'http://localhost:4200/login/callback',
+        scopes: ['openid', 'profile', 'email']
+    }
+    // oidc: {
+    //     clientId: '<<UPDATE-WITH-YOUR-APP-CLIENT-ID>>',
+    //     issuer: 'https://<<UPDATE-WITH-YOUR-DEV-DOMAIN>>/oauth2/default',
+    //     redirectUri: 'http://localhost:4200/login/callback',
+    //     scopes: ['openid', 'profile', 'email']
+    // }
+
+}
